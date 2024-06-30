fortuneOS-AOSP/device_fortune_sepolicy

fixup! sepolicy: Add google_battery service policies by default
* Do not include these on Pixel devices

Change-Id: I33c90d0210d71f253f2455956001e7101a0028f8

common/sepolicy.mk
@@ -38,6 +38,14 @@ ifeq ($(TARGET_INCLUDE_PIXEL_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
     device/parasite/sepolicy/common/private/google
 
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/parasite/sepolicy/common/system/google
+else
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/parasite/sepolicy/common/vendor/google
+endif
+
 # Flipendo
 BOARD_SEPOLICY_DIRS += \
     hardware/google/pixel-sepolicy/flipendo

The-Pixel-Project/device_aosp_sepolicy

common/sepolicy.mk
@@ -39,6 +39,14 @@ ifeq ($(TARGET_INCLUDE_PIXEL_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
     device/custom/sepolicy/common/private/google
 
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/custom/sepolicy/common/system/google
+else
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/custom/sepolicy/common/vendor/google
+endif
+
 # Flipendo
 BOARD_SEPOLICY_DIRS += \
     hardware/google/pixel-sepolicy/flipendo